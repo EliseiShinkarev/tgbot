f25bbfc6143f3f5dccc8e4fac4077f6764762a2f
EliseiShinkarev <104735051+EliseiShinkarev@users.noreply.github.com>
2022-12-23 17:35:52 +0300

    Add files via upload

---
+++ bot_exchanger/bot/keyboards/callback.py
@@ -0,0 +1,7 @@
+from aiogram.utils.callback_data import CallbackData
+
+user_menu_callback = CallbackData("user_menu", "option")
+admin_menu_callback = CallbackData("admin_menu", "option")
+cancel_callback = CallbackData("cancel")
+currency_menu_symbol_callback = CallbackData("currency_menu_symbol", "symbol")
+currency_menu_page_callback = CallbackData("currency_menu_page", "page")
